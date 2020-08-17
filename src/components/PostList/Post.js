ba81f54729ecc40d5cf301c70cc2410781e6eab0
nayanjyotisharma675@gmail.com <nayanjyotisharma675@gmail.com>
2020-08-17 14:46:34 +0530

    changed delete post

--- src/components/PostList/Post.js
+++ src/components/PostList/Post.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React         from 'react';
+import DeleteIcon    from '@material-ui/icons/Delete';
+import Grid          from '@material-ui/core/Grid';
+import Paper         from '@material-ui/core/Paper';
+import './post.css';
 
 const Post = (props) => {
-    return (
-    <>
-        <h3>{props.post.text}</h3>
-        <Button variant='outline-primary' onClick={()=>props.delete(props.post.id)}>Delete</Button>
-    </>
-    );
+	return (
+		<Grid className = 'delete-post'>
+			<Paper elevation = { 3 }>
+				<Grid className ='delete-post-body'  container>
+					<Grid xs = { 10 } item>
+						{props.post.text}
+					</Grid>
+					<Grid variant='outline-primary' onClick={()=>props.delete(props.post.id)} xs = { 2 } align='right' item>
+						<DeleteIcon/>
+					</Grid>
+				</Grid>
+			</Paper>
+		</Grid>
+		);
 }
- 
+
 export default Post;
